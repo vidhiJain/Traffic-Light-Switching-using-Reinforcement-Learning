401c5a06b6ed8999e9913ef5b426c46eeee528f8
Prakhar Agarwal <prakharagarwal2511@gmail.com>
2020-02-16 08:53:58 -0800

    Add files via upload

---
+++ utils/Velocity.java
@@ -0,0 +1,29 @@
+package utils;
+
+
+//Utility class to represent the x and y velocities of an object.
+public class Velocity {
+    private int x;
+    private int y;
+
+    public Velocity(int xSpeed, int ySpeed) {
+        this.x = xSpeed;
+        this.y = ySpeed;
+    }
+
+    public int getXSpeed() {
+        return x;
+    }
+
+    public int getYSpeed() {
+        return y;
+    }
+
+    public void setXSpeed(int xSpeed) {
+        x = xSpeed;
+    }
+
+    public void setYSpeed(int ySpeed) {
+        y = ySpeed;
+    }
+}
